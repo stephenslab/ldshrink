6b390ed72b82248fd9f22cd4b2d18ea134759238
Nicholas Knoblauch <nwknoblauch@gmail.com>
2017-09-19 12:00:50 -0500

    simulate and estimate beta at given loci

--- inst/snakemake_files/scripts/map_betahat_se.R
+++ inst/snakemake_files/scripts/map_betahat_se.R
@@ -5,12 +5,19 @@ library(RSSp)
 
 gdsf <- snakemake@input[["gdsf"]]
 outf <- snakemake@output[["sumstatf"]]
+ymatf <- snakemake@input[["ymatf"]]
 ymat <- read_2d_mat_h5(snakemake@input[["ymatf"]],
                        "trait",
                        "ymat")
 
 
+
 gds <- seqOpen(gdsf, readonly = T)
+h5grps <- h5ls(ymatf)
+if(sum(grepl("SNPinfo",h5grps))){
+    snpi_df <- read_df_h5(ymatf,"SNPinfo")
+    subset_gds(gds,snpi_df)
+}
 
 
 map_bh_se_gds(gds,ymat,out_file=outf)
