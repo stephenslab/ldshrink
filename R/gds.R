27cf53d5e674493adf20ff4956163ce74afad155
Nicholas Knoblauch <nwknoblauch@gmail.com>
2017-10-09 16:12:13 -0500

    added ldetect breaks

--- R/gds.R
+++ R/gds.R
@@ -27,7 +27,7 @@ read_SNPinfo_gds <- function(gds,alleles=F,MAF=F,region_id=F){
 subset_gds <- function(gds,info_df,region_id=F){
   si_df <- dplyr::inner_join(info_df, read_SNPinfo_gds(gds,region_id=region_id)) %>% 
     dplyr::distinct(snp_id, .keep_all = T) %>% dplyr::arrange(snp_id)
-  seqSetFilter(gds,variant.sel = si_df$snp_id)
+  SeqArray::seqSetFilter(gds,variant.sel = si_df$snp_id)
   return(si_df)
 }
 
@@ -45,11 +45,11 @@ mult_eigen_chunks <- function(chunk_evdf,uhmat){
 
 
 read_SNPinfo_ldsc <- function(gds){
-  return(tibble::data_frame(CHR=seqGetData(gds,var.name="chromosome"),
-                            SNP=seqGetData(gds,var.name="annotation/id"),
-                            BP=seqGetData(gds,var.name="position"),
-                            CM=seqGetData(gds,var.name="annotation/info/map"),
-                            MAF=seqAlleleFreq(gds)))
+  return(tibble::data_frame(CHR=SeqArray::seqGetData(gds,var.name="chromosome"),
+                            SNP=SeqArray::seqGetData(gds,var.name="annotation/id"),
+                            BP=SeqArray::seqGetData(gds,var.name="position"),
+                            CM=SeqArray::seqGetData(gds,var.name="annotation/info/map"),
+                            MAF=SeqArray::seqAlleleFreq(gds)))
 }
 
 
@@ -60,6 +60,37 @@ read_SNPinfo_ldsc <- function(gds){
 is_SNV <- function(allelevec){
   stopifnot(is.character(allelevec))
   return(sapply(strsplit(allelevec,split=",",fixed=T,useBytes = T),function(x){max(sapply(x,nchar))})==1)
+}
+
+
+
+seqIMPUTE2GDS <- function(hap.fn,leg.fn,sample.fn,map.fn,out.gdsfn,chrom,compress.geno="LZ4_RA.fast",compress.annotation="LZ4_RA.fast"){
+  
+  map_dat <- readr::read_delim(map.fn,delim=" ",col_names = c("ID","pos","map")) %>% dplyr::mutate(chrom=chrom)
+  sample.id <- scan(sample.fn,what=character(),sep="\n")
+  sample.id <- c(sapply(sample.id,function(x)c(paste0(x,"-1"),paste0(x,"-2"))))
+  leg_df <- readr::read_delim(leg.fn,delim=" ") %>% dplyr::mutate(allele=paste0(allele0,",",allele1),chrom=chrom)
+  geno_mat <- readr::read_delim(hap.fn,delim=" ",col_names = sample.id) %>% dplyr::mutate(ID=leg_df$ID) 
+  leg_df <- dplyr::inner_join(leg_df,map_dat)
+  geno_mat <- dplyr::semi_join(geno_mat,leg_df)
+  geno_mat <- data.matrix(dplyr::select(geno_mat,-ID))
+  p <- nrow(geno_mat)
+  tfile <- tempfile()
+  SNPRelate::snpgdsCreateGeno(gds.fn = tfile,genmat = geno_mat,
+                   sample.id = sample.id,
+                   snp.id = 1:p,
+                   snp.rs.id = leg_df$ID,
+                   snp.chromosome = leg_df$chrom,
+                   snp.position = leg_df$pos,
+                   snp.allele = leg_df$allele,
+                   compress.annotation = compress.annotation,
+                   compress.geno = compress.geno)
+
+  SeqArray::seqSNP2GDS(gds.fn =tfile ,out.fn =out.gdsfn ,storage.option ="LZ4_RA.fast")
+  gds <- SeqArray::seqOpen(out.gdsfn,readonly = F)
+  gdsfmt::add.gdsn(gdsfmt::index.gdsn(gds,"annotation/info"),"map",leg_df$map,replace=T,compress="LZ4_RA.fast")  
+  SeqArray::seqClose(gds)
+
 }
 
 
@@ -81,16 +112,16 @@ import_panel_data <- function(temp_gds=NULL,
     stopifnot(overwrite) 
     file.remove(output_file)
   }
-  seqMerge(temp_gds,output_file,storage.option="LZ4_RA.fast")
+ SeqArray::seqMerge(temp_gds,output_file,storage.option="LZ4_RA.fast")
   
-  gds <- seqOpen(output_file,readonly = F)
+  gds <-SeqArray::seqOpen(output_file,readonly = F)
   leg_df <- read_SNPinfo_gds(gds) %>% dplyr::left_join(map_df)
-  stopifnot(nrow(leg_df)==length(seqGetData(gds,"variant.id")))
+  stopifnot(nrow(leg_df)==length(SeqArray::seqGetData(gds,"variant.id")))
 
 
-  gdsfmt::add.gdsn(index.gdsn(gds,"annotation/info"),"map",leg_df$map,replace=T,compress="LZ4_RA.fast")
+  gdsfmt::add.gdsn(gdsfmt::index.gdsn(gds,"annotation/info"),"map",leg_df$map,replace=T,compress="LZ4_RA.fast")
   cat("Adding Chunk Delimiters\n")
-  seqClose(gds)
+ SeqArray::seqClose(gds)
   add_chunk_gds(output_file,ld_break_file)
 }
 
@@ -98,14 +129,14 @@ import_panel_data <- function(temp_gds=NULL,
 add_chunk_gds <- function(gds_file,region_bed_file){
 
   stopifnot(file.exists(region_bed_file),file.exists(gds_file))
-  gds <- seqOpen(gds_file,readonly = F)
+  gds <- SeqArray::seqOpen(gds_file,readonly = F)
   region_bed <- readr::read_delim(region_bed_file,delim="\t",trim_ws = T) %>% dplyr::mutate(range_id=as.character(1:n()),chr=gsub("chr","",chr))
 #  region_range <- GenomicRanges::GRanges(seqnames=region_bed$chr,ranges=IRanges::IRanges(start=region_bed$start,end = region_bed$stop))
   snp_df <- read_SNPinfo_gds(gds) %>% dplyr::mutate(snp_id=as.character(snp_id))
   match_df <- match_SNP(region_bed,snp_df) %>% dplyr::arrange(as.integer(snp_id))
   stopifnot(all(snp_df$snp_id==match_df$snp_id))
-  gdsfmt::add.gdsn(index.gdsn(gds,"annotation/info"),"LD_chunk",as.integer(match_df$range_id),replace=T,compress="LZ4_RA.fast")
-  seqClose(gds)
+  gdsfmt::add.gdsn(gdsfmt::index.gdsn(gds,"annotation/info"),"LD_chunk",as.integer(match_df$range_id),replace=T,compress="LZ4_RA.fast")
+  SeqArray::seqClose(gds)
 }
 
 calc_LD_gds <- function(gds,m=85,Ne=11490.672741,cutoff=1e-3){
@@ -162,24 +193,24 @@ chunkwise_LDshrink_ldsc <- function(gds_file,chrom=19,out_dir=NULL,m=85,Ne=11490
             !is.null(out_dir),dir.exists(out_dir))
   outfile <- file.path(out_dir,paste0(chrom,".l2.ldscore.gz"))
   soutfile <- file.path(out_dir,paste0(chrom,".l2.M_5_50"))
-  gds <- seqOpen(gds_file,readonly = T)
-  # chroms <- seqGetData(gds,"chromosome")
-  seqSetFilterChrom(gds,include=as.character(chrom))
-  LD_chunks <- seqGetData(gds,var.name="annotation/info/LD_chunk")
+  gds <-SeqArray::seqOpen(gds_file,readonly = T)
+  # chroms <-SeqArray::seqGetData(gds,"chromosome")
+ SeqArray::seqSetFilterChrom(gds,include=as.character(chrom))
+  LD_chunks <-SeqArray::seqGetData(gds,var.name="annotation/info/LD_chunk")
   region_ids <- unique(LD_chunks)
-  mfilt <- seqGetFilter(gds)
+  mfilt <-SeqArray::seqGetFilter(gds)
   resl <- list()
   # pb <- progress::progress_bar$new(total=length(region_ids))
   for(i in 1:length(region_ids)){
     region_id <- region_ids[i]
     tr <- LD_chunks %in% region_id
-    seqSetFilter(gds,variant.sel=tr,action="push+intersect")
+   SeqArray::seqSetFilter(gds,variant.sel=tr,action="push+intersect")
     si <- read_SNPinfo_ldsc(gds) 
     R <- calc_LD_gds(gds,m = m,Ne = Ne,cutoff = cutoff)
     si <- dplyr::mutate(si,L2=colSums(R^2)-1)
     resl[[i]] <- si
-    seqSetFilter(gds,action="pop")
-    tfilt <- seqGetFilter(gds)
+   SeqArray::seqSetFilter(gds,action="pop")
+    tfilt <-SeqArray::seqGetFilter(gds)
     stopifnot(all.equal(mfilt,tfilt))
     # pb$tick()
   }
