dc707e71677e11fb6cdbecb855649d5128786b4a
CreRecombinase <nwknoblauch@gmail.com>
2018-10-05 14:40:44 -0500

    add tidyr dependency

--- tests/testthat/test_util.R
+++ tests/testthat/test_util.R
@@ -45,17 +45,21 @@ test_that("flip all alleles for the same dataframe (but flipped)",{
   expect_true(all(flip_allele_exp(snp_df$allele,ld_df$allele)))
 })
 
-test_that("report NA when alleles don't match",{
+test_that("report NA when alleles don't match", {
   p <- 100
-  sample_df <- tibble::data_frame(ref=sample(c("A","C","G","T"),100,replace=T),alt=sample(c("A","C","G","T"),100,replace=T)) %>% 
-    dplyr::filter(ref!=alt) %>% dplyr::sample_n(p,replace=T) 
-  snp_df <- sample_df %>%tidyr::unite(allele,sep=",")
+  sample_df <- tibble::data_frame(ref=sample(c("A", "C", "G", "T"), 100, replace=T),
+                                  alt=sample(c("A", "C", "G", "T"), 100, replace=T)) %>%
+      dplyr::filter(ref!=alt) %>%
+      dplyr::sample_n(p, replace=T)
+  snp_df <- sample_df %>%
+      tidyr::unite(allele, sep=",")
   sample_df$ref[4] <- "F"
   sample_df$alt[6] <- "I"
-  ld_df <- dplyr::select(sample_df,alt,ref) %>%tidyr::unite(allele,sep=",")
-  ret <- flip_allele_exp(snp_df$allele,ld_df$allele)
-  expect_true(all(ret[-c(4,6)]))
-  expect_true(all(is.na(ret[c(4,6)])))
+  ld_df <- dplyr::select(sample_df, alt, ref) %>%
+      tidyr::unite(allele, sep=",")
+  ret <- flip_allele_exp(snp_df$allele, ld_df$allele)
+  expect_true(all(ret[-c(4, 6)]))
+  expect_true(all(is.na(ret[c(4, 6)])))
 })
 
 
